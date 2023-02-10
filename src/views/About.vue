d874eb894742e3a63c8080adb676349eeeccde3e
Kenny Wong <lizewkn@gmail.com>
2023-02-11 00:19:27 +0800

    text

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,7 +4,10 @@ import PortfolioItem from './Portfolio.vue'
 
 <template>
   <main>
-    
+    <p>About myself qualification experience and training: I have a BS in Engineering from the University of Colorado Boulder, where last year I received my MS degree. With this background as well i believe I will enjoy more technical work with engineers such companies like Boeing or General Motors which make highly accurate cars that use many sensors to improve efficiency and safety.
+
+
+After college, after graduating high school was all about being able see future opportunities on engineering jobs agains new tech start-ups! Being interested both at personal, technological and business level is an asset for me while simultaneously not putting up too much effort around technology research projects (most interesting topic right now).</p>
     <PortfolioItem/>
   </main>
 </template>
