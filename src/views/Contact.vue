54ae2fde1f0710ba9862c668409ef4d2a2a8bb90
Kenny Wong <lizewkn@gmail.com>
2023-02-14 18:32:58 +0800

    update

---
+++ src/views/Contact.vue
@@ -0,0 +1,57 @@
+<template>
+  <main>
+    <p>About myself qualification experience and training: I have a BS in Engineering from the University of Colorado Boulder, where last year I received my MS degree. With this background as well i believe I will enjoy more technical work with engineers such companies like Boeing or General Motors which make highly accurate cars that use many sensors to improve efficiency and safety.
+
+
+After college, after graduating high school was all about being able see future opportunities on engineering jobs agains new tech start-ups! Being interested both at personal, technological and business level is an asset for me while simultaneously not putting up too much effort around technology research projects (most interesting topic right now).</p>
+
+<p>
+Summary
+Hardworking and driven sales professional with more than 10 years of experience seeking an account management position in the healthcare industry.
+
+Areas of Expertise
+Medical Device, Supplies & Pharmaceutical Sales
+I have a wealth of experience in selling to healthcare organizations ranging from large hospitals to small private practices. In previous roles, I’ve managed prospecting efforts, relationship development, new client onboarding and account management within both the medical device and pharmaceutical product verticals.
+
+Relationship Management
+I am skilled in developing and new relationships with prospects and nurturing relationships with existing clients. In previous roles, I used a combination of proficiency in conflict resolution and my ability to build rapport to increase client retention rates as high as 300% year over year.
+
+Sales Team Leadership
+I have managed a sales team of more than 10 sales associates at a time, coached and mentored junior sales representatives and regularly lead teams to exceed monthly, quarterly and yearly quotas.
+
+Professional Experience
+Creative Director
+ABC Co., 2013–2015
+
+Manage a team of 12 creatives, including designers and copywriters.
+Oversee all in-house creative projects and ensure all deliverables meet brand guidelines.
+Senior Graphic Designer
+ABC Company, 2013–2015
+
+Designed creative for all digital properties.
+Spearheaded website redesign results in a 40% improvement in bounce rate.
+Developed in-house brand style guide currently used by the entire creative department.
+Graphic Designer
+XYZ Creative Agency, 2010–2013
+
+Develop visual concepts for web and print design, including websites, mobile sites, digital ads, business cards and trade show collateral.
+Related Skills
+Team Management
+Coordinate team of creative resources, lead team meetings and offer to mentor as needed.
+
+Project Management
+Manage all aspects of creative projects, including timeline, resource coordination, internal communication and sharing progress reports with outside stakeholders.
+
+Branding
+Create logos, design brand marks, offer brand color recommendations and create style guides to ensure cohesiveness across all assets.
+
+Additional Skills
+Illustration, Typography, Client Communication, Time Management, Mobile Design, Adobe Creative Suite
+
+Education
+University of California, San Diego
+Bachelor of Art in Advertising, Certificate in Graphic Design</p>
+
+    <PortfolioItem/>
+  </main>
+</template>
