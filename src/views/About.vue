ed0f846d3ce6585d6887e467b38d71c1eb527238
Kenny Wong <lizewkn@gmail.com>
2023-04-20 17:07:09 +0800

    update

--- src/views/About.vue
+++ src/views/About.vue
@@ -11,45 +11,65 @@ import PortfolioItem from './Portfolio.vue'
 
             <h5 class="fst-italic fw-lighter my-2">(03/2018 - 03/2023)</h5>
 
-            <p>At Flamma Works Design Limited, I work as a Senior Front-end Developer, and I have been in the role since
-            March 2018. In my time here, I have been responsible for the front-end programming of key projects such as
-            CitiBank. I have worked on various aspects of this project, including the development of electronic direct
-            mailers (edms), forms, standalone pages, and campaign sites.</p>
+            <p>As a Senior Front-end Developer at Flamma Works Design Limited since March 2018, my primary responsibility has been the front-end programming of key projects, including the CitiBank project. I have been involved in various aspects of this project, such as the development of electronic direct mailers (edms), forms, standalone pages, animations, videos, and campaign sites.
+<br>
+To complete these tasks, I have utilized a range of tools and technologies, including Adobe Creative Suite, HTML, CSS, PHP, JavaScript, GitHub, Angular, Vue, jQuery, TypeScript, and Bootstrap. These tools have allowed me to effectively design and develop visually appealing and user-friendly websites and apps that meet the client's requirements.
+<br>
+In addition to my technical responsibilities, I have also worked closely with vendors to ensure timely and efficient project delivery. My effective communication and collaboration skills have been essential in ensuring that projects are completed on time and within budget.
+<br>
+Overall, my experience as a Senior Front-end Developer at Flamma Works Design Limited has provided me with a wealth of experience and knowledge in web design and development. My technical expertise, combined with my strong communication and collaboration skills, has allowed me to successfully contribute to various web-based projects and meet the high standards expected by clients.</p>
 
             <h3 class="green mt-5">Web Designer <span class="fs-5">Lazybug Studio</span></h3>
 
               <h5 class="fst-italic fw-lighter my-2">(01/2016 - 12/2017)</h5>
 
-              <p>As a Web Designer at Lazybug Studio from January 2016 to December 2017, I was responsible for designing and
-            developing a range of web and app-based projects. I worked on a range of different aspects of each project,
-            including the user interface (UI), graphics, logo design, and front-end programming. Some of the key projects
-            I worked on during my time at Lazybug Studio include lazybugstudio and sagaming.</p>
+              <p>As a Web Designer at Lazybug Studio, my responsibilities included designing and developing various web and app-based projects. I was involved in several aspects of each project, such as UI/UX design, interactive website development, designing the UI, creating graphics and logos, and front-end programming.
+<br>
+To complete my tasks, I utilized a range of tools and technologies, including Adobe Creative Suite, HTML, CSS, PHP, JavaScript, jQuery, Wordpress, Bootstrap, and Figma. Notably, I worked on key projects such as the design and development of the company's website, lazybugstudio.com, and the sagaming website.
+<br>
+As a Web Designer, I also had the responsibility of presenting design ideas and concepts to clients, which necessitated excellent communication skills and the ability to understand the client's specific needs and preferences. I then applied my expertise in design and development to create unique and engaging websites and apps that effectively conveyed the client's message and brand.
+<br>
+My experience at Lazybug Studio enabled me to develop a versatile skill set in web design and development, as well as effective communication and collaboration skills. I learned how to work in a team environment and adapt quickly to changing project requirements. These skills have been invaluable in my career as a web designer and have allowed me to successfully contribute to various web and app-based projects.</p>
 
             <h3 class="green mt-5">Web/App Designer <span class="fs-5">ON.CC</span></h3>
 
               <h5 class="fst-italic fw-lighter my-2">(08/2015 - 12/2015)</h5>
 
-              <p>During my time as a Web/App Designer at ON.CC, I was responsible for designing and developing
-            a range of web and app-based projects. This included designing the user interface (UI), graphics, and logos.
-            One of the key projects I worked on during my time at ON.CC web design of the
-            ON.CC iPad App and the website itself.</p>
+              <p>In my role as a Web/App Designer at ON.CC, I was responsible for designing and developing a diverse range of web and app-based projects. This included designing the user interface (UI), graphics, and logos for each project.
+<br>
+During my tenure at ON.CC, I worked on several significant projects that enabled me to develop and showcase my skills in web design and development. One such project involved designing the ON.CC iPad App and website. To accomplish this, I utilized a range of tools and technologies such as Adobe Creative Suite, HTML, CSS, JavaScript, PHP, jQuery, Wordpress, Bootstrap, and MySQL to create a visually appealing and user-friendly design for both the app and website.
+<br>
+Another notable project I worked on was the design and development of the Hong Kong Jockey Club campaign websites. These websites were designed to be WCAG recognized, necessitating a meticulous approach to design and development.
+<br>
+In addition to my technical responsibilities, I also presented design ideas and concepts to clients. This required me to effectively communicate with clients to understand their specific needs and preferences. I then applied my design and development expertise to create unique and engaging websites and apps that conveyed the client's message and brand effectively.
+<br>
+Overall, my experience as a Web/App Designer at ON.CC allowed me to develop a versatile skill set that includes technical expertise in web and app design and development, as well as effective communication and collaboration skills. These skills will undoubtedly serve me well in any future roles I undertake in the field of web design and development.</p>
 
             <h3 class="green mt-5">Web Designer/Developer <span class="fs-5">Intimex Business Solutions</span></h3>
 
               <h5 class="fst-italic fw-lighter my-2">(03/2014 - 08/2015)</h5>
 
-            <p>As a Web Designer/Developer at Intimex Business Solutions, I was responsible for designing and
-            developing a range of websites. In addition to this, I also worked on various graphics and logo design
-            projects. During my time at Intimex Business Solutions, I was also responsible for the front-end programming
-            of the websites I worked on.</p>
+            <p>
+              As a Web Designer/Developer at Intimex Business Solutions, my role encompassed designing and developing a variety of websites, as well as undertaking graphics and logo design projects. A crucial aspect of my role was front-end programming for the websites and online stores that I worked on.
+<br>
+My responsibility extended to ensuring that the websites of government-funded organizations were WCAG compatible. This required me to possess an in-depth understanding of accessibility guidelines and to have a meticulous approach to web development. To accomplish this, I used a range of tools and technologies such as Adobe Creative Suite, HTML, CSS, .NET, C#, PHP, JavaScript, jQuery, Wordpress, Bootstrap, and MySQL to create visually appealing and user-friendly designs that adhered to the strict accessibility requirements of these clients.
+<br>
+Aside from my technical responsibilities, I also had to present design ideas and concepts to clients, necessitating excellent communication skills and the ability to comprehend their specific needs and preferences. I then utilized my expertise in design and development to create unique and captivating websites that effectively conveyed the client's message and brand.
+<br>
+In summary, my experience as a Web Designer/Developer at Intimex Business Solutions enabled me to develop a robust set of skills in both design and development, along with a deep understanding of accessibility guidelines and how to execute successful projects for government-funded organizations.
+            </p>
 
             <h3 class="green mt-5">Web Programmer <span class="fs-5">Springtime Design</span></h3>
 
               <h5 class="fst-italic fw-lighter my-2">(05/2013 - 12/2013)</h5>
 
-              <p>As a Web Programmer at Springtime Design, I was responsible for the front-end programming of
-          various websites. This included working on different aspects of each project, including the development of the
-          user interface (UI) and other front-end programming tasks.</p>
+          <p>As a Web Programmer at Springtime Design, I was responsible for various front-end programming tasks related to web development projects. My role involved working on different aspects of each project to ensure that the websites were visually appealing and user-friendly.
+<br>
+One of my primary responsibilities was developing the user interface (UI) for these websites. To achieve this, I utilized a range of tools such as Adobe Creative Suite, HTML, CSS, .NET, and C#. Effective communication with team members was crucial to ensure that the design and functionality of each website met the client's expectations.
+<br>
+Apart from UI development, I also undertook other front-end programming tasks, including optimizing websites for search engines, ensuring accessibility for users with disabilities, and ensuring compatibility with various devices and browsers.
+<br>
+My experience as a Web Programmer at Springtime Design helped me develop a versatile skill set in front-end programming, as well as a deep understanding of creating engaging and effective user interfaces for websites.</p>
       </div>
     </div>
     <!-- <div class="row mt-4">
