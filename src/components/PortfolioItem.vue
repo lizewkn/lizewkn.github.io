93ab2f48f8853d752778ebaf583c2e1976853e40
Kenny Wong <lizewkn@gmail.com>
2023-10-31 18:26:30 +0800

    update

--- src/components/PortfolioItem.vue
+++ src/components/PortfolioItem.vue
@@ -652,7 +652,7 @@
         </div>
       </div>
     </div>
-  <div class="modal fade" id="item18" data-bs-keyboard="false" tabindex="-1" aria-labelledby="item18Label"
+  <div class="modal fade" id="item18" data-bs-keyboard="false" tabindex="-1" aria-labelledby="item17Label"
     aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-xl border-0">
       <div class="modal-content">
