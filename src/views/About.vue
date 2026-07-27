f56cdcc6283d7275037cc7cb48249774240de2ad
DESKTOP-1PERJF8\user <kenny.kn.wong@pccw.com>
2026-07-28 00:30:22 +0800

    restore 3 work experiences: Freelance Developer, Intimex Business Solutions, Springtime Design

--- src/views/About.vue
+++ src/views/About.vue
@@ -28,6 +28,12 @@ Vendor & Technical Planning: Conducted system analysis, architecture design, and
 
             <p>Executed user interface design and front-end development strategies to optimize performance and usability across corporate travel applications. Collaborated on software system analysis to map out engineering requirements and streamline data integration pipelines.</p>
 
+           <h3 class="green mt-5">Freelance Developer</h3>
+
+            <h5 class="fst-italic fw-lighter my-2">(03/2023 - 11/2023)</h5>
+
+            <p>Since March 2023, I've been working as a freelance developer, specializing in web app programming, UI/UX design, web design, online store design, and programming. I've also integrated technologies like Magento, Angular, React, and Vue into my projects. Recently, I've expanded my expertise to include product design and 3D printing, allowing me to offer comprehensive solutions that merge digital and physical realms. This additional dimension enables me to assist clients in conceptualizing and prototyping physical products with both functionality and aesthetics in mind. I've managed online stores proficiently, implementing content management systems for efficient website maintenance. This role has also equipped me with valuable project management skills, allowing me to independently oversee projects from start to finish while staying updated with the latest trends in web development and design.</p>
+
 <h3 class="green mt-5">Senior Frontend Developer <span class="fs-5">Flamma Works</span></h3>
 
             <h5 class="fst-italic fw-lighter my-2">(03/2018 - 03/2023)</h5>
@@ -49,6 +55,32 @@ Utilized tools and technologies including Adobe Creative Suite, HTML, CSS, PHP, 
               <h5 class="fst-italic fw-lighter my-2">(07/2015 - 12/2015)</h5>
 
               <p>Responsible for designing and developing a diverse range of web and app-based projects, including designing the user interface (UI), graphics, and logos. Worked on the ON.CC iPad App and website using Adobe Creative Suite, HTML, CSS, JavaScript, PHP, jQuery, WordPress, Bootstrap, and MySQL.</p>
+
+            <h3 class="green mt-5">Web Designer/Developer <span class="fs-5">Intimex Business Solutions</span></h3>
+
+              <h5 class="fst-italic fw-lighter my-2">(03/2014 - 08/2015)</h5>
+
+            <p>
+              As a Web Designer/Developer at Intimex Business Solutions, my role encompassed designing and developing a variety of websites, as well as undertaking graphics and logo design projects. A crucial aspect of my role was front-end programming for the websites and online stores that I worked on.
+<br>
+My responsibility extended to ensuring that the websites of government-funded organizations were WCAG compatible. This required me to possess an in-depth understanding of accessibility guidelines and to have a meticulous approach to web development. To accomplish this, I used a range of tools and technologies such as Adobe Creative Suite, HTML, CSS, .NET, C#, PHP, JavaScript, jQuery, WordPress, Bootstrap, and MySQL to create visually appealing and user-friendly designs that adhered to the strict accessibility requirements of these clients.
+<br>
+Aside from my technical responsibilities, I also had to present design ideas and concepts to clients, necessitating excellent communication skills and the ability to comprehend their specific needs and preferences. I then utilized my expertise in design and development to create unique and captivating websites that effectively conveyed the client's message and brand.
+<br>
+In summary, my experience as a Web Designer/Developer at Intimex Business Solutions enabled me to develop a robust set of skills in both design and development, along with a deep understanding of accessibility guidelines and how to execute successful projects for government-funded organizations.
+            </p>
+
+            <h3 class="green mt-5">Web Programmer <span class="fs-5">Springtime Design</span></h3>
+
+              <h5 class="fst-italic fw-lighter my-2">(05/2013 - 12/2013)</h5>
+
+          <p>As a Web Programmer at Springtime Design, I was responsible for various front-end programming tasks related to web development projects. My role involved working on different aspects of each project to ensure that the websites were visually appealing and user-friendly.
+<br>
+One of my primary responsibilities was developing the user interface (UI) for these websites. To achieve this, I utilized a range of tools such as Adobe Creative Suite, HTML, CSS, .NET, and C#. Effective communication with team members was crucial to ensure that the design and functionality of each website met the client's expectations.
+<br>
+Apart from UI development, I also undertook other front-end programming tasks, including optimizing websites for search engines, ensuring accessibility for users with disabilities, and ensuring compatibility with various devices and browsers.
+<br>
+My experience as a Web Programmer at Springtime Design helped me develop a versatile skill set in front-end programming, as well as a deep understanding of creating engaging and effective user interfaces for websites.</p>
       </div>
     </div>
     <!-- <div class="row mt-4">
